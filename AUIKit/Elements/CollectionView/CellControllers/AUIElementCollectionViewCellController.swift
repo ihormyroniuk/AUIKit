d894f9e58fd5aab45d494e3cf5b23a520f8452c5
Ihor Myroniuk <ihor.myroniuk@brander.ua>
2019-02-15 13:39:09 +0200

    Make didSelectDelegate weak in AUIElementCollectionViewCellController

--- AUIKit/Elements/CollectionView/CellControllers/AUIElementCollectionViewCellController.swift
+++ AUIKit/Elements/CollectionView/CellControllers/AUIElementCollectionViewCellController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 open class AUIElementCollectionViewCellController: AUICollectionViewCellController {
   
-  open var didSelectDelegate: AUICollectionViewCellControllerDelegate?
+  open weak var didSelectDelegate: AUICollectionViewCellControllerDelegate?
   
   open var view: UIView?
   
