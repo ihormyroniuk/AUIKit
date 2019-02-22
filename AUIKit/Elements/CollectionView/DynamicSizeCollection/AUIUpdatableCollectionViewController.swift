0c71e7efc9994b2683e1dbf6a118a1d97bc576bb
Aleksandr Orlov <alexandr.orlov@brander.ua>
2019-02-22 14:30:10 +0200

    Removed calling didEndDisplayCell for deleted cell controllers.

--- AUIKit/Elements/CollectionView/DynamicSizeCollection/AUIUpdatableCollectionViewController.swift
+++ AUIKit/Elements/CollectionView/DynamicSizeCollection/AUIUpdatableCollectionViewController.swift
@@ -158,7 +158,6 @@ extension AUIUpdatableCollectionViewController: AUICollectionViewDelegateProxyDe
   open func collectionView(_ collectionView: UICollectionView, didEndDisplaying cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
     guard !indexPath.isEmpty else { return }
     if let deletedCellController = deletedCellControllers[indexPath] {
-      deletedCellController.didEndDisplayCell()
       deletedCellControllers.removeValue(forKey: indexPath)
     } else {
       cellControllers[indexPath.row].didEndDisplayCell()
